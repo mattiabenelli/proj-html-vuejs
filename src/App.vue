09ef008a3d5943c21645bd18e9cf45a7b70e1ea3
mattiabenelli <benellimattia0@gmail.com>
2023-01-23 11:52:27 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components:{
+    HelloWorld
+  }
+}
+</script>
+
+<template lang="">
+<HelloWorld />
+</template>
+
+<style lang="scss">
+  @use './styles/generals.scss' as *
+</style>
