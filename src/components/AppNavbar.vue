914c5a7ed109c39303098741213274e1114c3761
mattiabenelli <benellimattia0@gmail.com>
2023-01-24 23:16:44 +0100

    dropdown funzionante

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,10 +1,9 @@
 <script>
 export default {
-    props:{
-        navItems: Array,
-    },
-  }
-
+  props: {
+    navItems: Array,
+  },
+};
 </script>
 
 <template lang="">
@@ -14,10 +13,10 @@ export default {
           <img src="/public/img/logo.png" alt="">
           <div class="nav-links">
             <ul class="row">
-              <li v-for="(item,index) in navItems" :key="index">
+              <li class="dropdown" v-for="(item,index) in navItems" :key="index">
                 <a :href="item.url">{{ item.label }}<span v-if="item.new" class="newSection">new</span></a>
-                <ul v-if="item.subMenu">
-                  <!-- <li v-for="(items,index) in item.subMenu" :key="index">{{ items }}</li> -->
+                <ul class="dropdown-content" v-if="item.subMenu">
+                  <li v-for="(items,index) in item.subMenu" :key="index">{{ items }}</li>
                 </ul>
               </li>
               <li><button class="blue-red-btn btn-small">schedule a workout</button></li>  
@@ -31,27 +30,49 @@ export default {
 </template>
 
 <style lang="scss">
-    @use '../styles/partials/variables' as * ;
-    @use '../styles/partials/mixins' as * ;
-  
-    .nav-links{
-    .row{
-      gap: 50px;
-    }
-    ul{
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+.nav-links {
+  .row {
+    gap: 50px;
+  }
+  ul {
     list-style-type: none;
-      a{
-        color: $SmallTxtFontColor;
-        text-decoration: none;
-    }
-    }
-    .newSection{
-      background-color: yellow;
-      padding: 1px 3px 4px 3px;
-      color: black;
-      border-radius: 5px;
-      margin-left: 5px;
+    a {
+      color: $SmallTxtFontColor;
+      text-decoration: none;
     }
   }
-    
+  .newSection {
+    background-color: yellow;
+    padding: 1px 3px 4px 3px;
+    color: black;
+    border-radius: 5px;
+    margin-left: 5px;
+  }
+}
+.dropdown {
+  position: relative;
+  display: inline-block;
+  height: 100%;
+}
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  // margin-top: 20px;
+}
+.dropdown-content li {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+.dropdown:hover .dropdown-content {
+  display: block;
+}
 </style>
