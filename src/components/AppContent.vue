eef32dc8c2f667170d699c695641c3a9fa3b90f0
mattiabenelli <benellimattia0@gmail.com>
2023-01-25 12:52:46 +0100

    aggiunte card in "sezione arcobaleno"

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,17 +1,59 @@
 <script>
+import AppcardRainbow from "../components/AppcardRainbow.vue";
 export default {
-  
-}
+  components: {
+    AppcardRainbow,
+  },
+  data() {
+    return {
+      cardsRainbow: [
+        {
+          img: "/public/img/service6-2x.jpg",
+          url: "#",
+          title: "Crossfit workout",
+          subtitle: "Push your limit",
+        },
+        {
+          img: "/public/img/box1-2x.jpg",
+          url: "#",
+          title: "New gym apparel",
+          subtitle: "Look good, feel good",
+        },
+        {
+          img: "/public/img/box3-2x.jpg",
+          url: "#",
+          title: "Team training",
+          subtitle: "Find a partner",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template lang="">
-  <div>
-
-  </div>
+  <main>
+    <div class="rainbow-section">
+      <div class="wrapper">
+        <div class="card section">
+          <div class="row space-bwt">
+            <AppcardRainbow :cardRainbow="cardsRainbow"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
-  @use '../styles/partials/variables' as * ;
-  @use '../styles/partials/mixins' as * ;
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
 
+.rainbow-section {
+  background: linear-gradient(135deg, #4257ef, #6b0c7a, #bc0428);
+  height: 400px;
+}
+.card-section {
+  width: 100%;
+}
 </style>
---
+++ src/components/AppcardRainbow.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  props: {
+    cardRainbow: Array,
+  },
+};
+</script>
+
+<template lang="">
+    <div v-for="(item,index) in cardRainbow" :key="index" class="card-rainbow">
+        <a :href="item.url"><img :src="item.img" alt=""></a>
+        <div class="card-rainbow-text">
+            <div class="card-rainbow-text-title">{{ item.title }}</div>
+            <div class="card-rainbow-text-subtitle">{{ item.subtitle }}</div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+.card-rainbow {
+  width: 30%;
+  position: relative;
+  a {
+    width: 100%;
+    height: 100%;
+  }
+  img {
+    width: 75%;
+    height: 75%;
+    box-shadow: 20px 20px 50px 10px black inset;
+  }
+  .card-rainbow-text {
+    position: absolute;
+    bottom: 30px;
+    left: 40px;
+    .card-rainbow-text-title {
+      color: white;
+      font-size: 25px;
+    }
+    .card-rainbow-text-subtitle {
+      color: $yellowPage;
+      font-size: 20px;
+    }
+  }
+}
+</style>
