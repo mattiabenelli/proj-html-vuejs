5b8c898ea89908314d401275927d717917574d48
mattiabenelli <benellimattia0@gmail.com>
2023-01-25 16:42:52 +0100

    prima parte sezione video done

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -52,17 +52,27 @@ export default {
         </div>
       </div>
     </div>
-    <div class="video-section">
+    <div class="video-section row space-bwt">
       <div class="youtube-section-video">
               <img src="public/img/service15-2x.jpg" alt="">
-            </div>
+              <button class="video-btn video-btn-main"><i class="fa-solid fa-play"></i></button>
+        </div>
         <div class="wrapper">
           <div class="youtube-section">
             <div class="youtube-section-text">
-
+              <div class="youtube-section-text-stitle">Tune up your workouts</div>
+              <div class="youtube-section-text-btitle">The best health & fitness advice online</div>
+              <div class="youtube-section-text-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam accusantium, quod culpa dolores adipisci provident aspernatur. Exercitationem, vero recusandae at sapiente laboriosam suscipit similique harum, culpa consectetur facilis maiores sint! </div>
+              <button class="yt-btn btn-big"><i class="icon fa-brands fa-youtube"></i>Visit my Youtube channel</button>
             </div>
         </div>
-        <div class="playlist-section"></div>
+      </div>
+    </div>
+    <div class="playlist-section">
+      <div class="wrapper">
+        <div class="playlist-tag">
+
+        </div>
       </div>
     </div>
   </main>
@@ -107,20 +117,46 @@ export default {
 }
 .video-section {
   .youtube-section-video {
-    width: 50%;
+    width: 70%;
     position: relative;
     img {
       width: 100%;
-      height: 800px;
+      height: 700px;
       object-fit: cover;
+      border-radius: 5px 5px 40px 5px;
+      transform: translateY(-50px);
+    }
+    .video-btn-main {
       position: absolute;
-      left: 0;
-      top: 0;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
     }
   }
   .youtube-section {
+    width: 80%;
+    margin: 0 auto;
     .youtube-section-text {
       height: 600px;
+      .youtube-section-text-stitle {
+        color: $blueBtn;
+        font-size: 25px;
+        font-weight: bold;
+        padding-bottom: 40px;
+      }
+      .youtube-section-text-btitle {
+        font-size: 70px;
+      }
+      .youtube-section-text-paragraph {
+        color: gray;
+        padding: 40px 0;
+        font-size: 20px;
+      }
+      .yt-btn {
+        background-color: #e20813;
+        border: #e20813;
+        color: white;
+      }
     }
   }
 }
